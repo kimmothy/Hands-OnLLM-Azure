19be137f2ba838032d1c5466b0128df56aee8a8f
kimmothy <kimmothy@naver.com>
2025-09-06 22:44:38 +0900

    인증 방식 변경

--- chapter01-Azure/python/deploy_project.py
+++ chapter01-Azure/python/deploy_project.py
@@ -1,7 +1,6 @@
-from azure.identity import DefaultAzureCredential
+from azure.identity import DefaultAzureCredential, InteractiveBrowserCredential
 from azure.mgmt.cognitiveservices import CognitiveServicesManagementClient
 from dotenv import load_dotenv
-
 
 subscription_id = load_dotenv("subs_id")
 resource_group_name = load_dotenv("rg_name")
@@ -9,8 +8,14 @@ foundry_resource_name = load_dotenv("ai_foundry_service_name")
 foundry_project_name = 'myprj'
 location = 'koreacentral'
 
+#########################################################################
+# Default Azure Credential 함수를 사용하면 실행 환경에 따라 가능한 인증방법을 사용하여 인증하게 됩니다.
+# terraform을 실행할 때 az login 명령을 실행했기 때문에 일반적으론 CLI 인증을 사용하게 됩니다.
+# 이 코드에선 인증 과정을 가시적으로 경험하기 위해 Interactive Brouser Credential을 사용합니다.
+#########################################################################
 client = CognitiveServicesManagementClient(
-    credential=DefaultAzureCredential(), 
+    # credential=DefaultAzureCredential(), 
+    credential=InteractiveBrowserCredential(), 
     subscription_id=subscription_id,
     api_version="2025-04-01-preview"
 )
